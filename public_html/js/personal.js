dba722732108523c76ef1764777c407ba4eb40bd
Ruslan Makarov <rmakarov@sprinterra.ru>
2024-11-20 17:28:57 +0300

    Dwaw dynamically table

--- public_html/js/personal.js
+++ public_html/js/personal.js
@@ -1,9 +1,40 @@
+const HEADER_TITLES =  {
+    fio: "ФИО",
+    speciality: "Должность",
+    phone: "Номер телефона"
+}
 
 async function drawPersonalTable() {
     let personal = await loadPersonal();
+    const personalTableContainer = document.getElementsByClassName('tableContainer')[0]
     const personalTable = document.createElement('table');
+    personalTable.classList.add("mainTable");
+    const personalTableHead = document.createElement('thead');
+    const personalTableBody = document.createElement('tbody');
+    const personalTableHeadRow = document.createElement('tr');
     console.log('personal[0]', personal[0])
 
+    Object.keys(personal[0]).forEach(function(key) {
+        const personalTableHeadColumn = document.createElement('th')
+        const headerTitleText = document.createTextNode(HEADER_TITLES[key]);
+        personalTableHeadColumn.appendChild(headerTitleText);
+        personalTableHeadRow.appendChild(personalTableHeadColumn);
+    });
+
+    personal.map((person) => {
+        const  personalTableBodyRow = document.createElement('tr');
+        Object.values(person).forEach(function(value) {
+            const personalTableBodyColumn = document.createElement('th')
+            const columnText = document.createTextNode(value);
+            personalTableBodyColumn.appendChild(columnText);
+            personalTableBodyRow.appendChild(personalTableBodyColumn);
+        });
+        personalTableBody.appendChild(personalTableBodyRow);
+    })
+    personalTableHead.appendChild(personalTableHeadRow);
+    personalTable.appendChild(personalTableHead);
+    personalTable.appendChild(personalTableBody);
+    personalTableContainer.appendChild(personalTable);
 }
 
 async function loadPersonal(){
