9e825be2caf43838d8d301909261459df7d6ef86
Ruslan Makarov <rmakarov@sprinterra.ru>
2024-11-20 10:49:08 +0300

    fetch personal

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,21 +1,32 @@
 
+let header = null;
+let headerContainer = null;
+let footer = null;
+let footerContainer = null;
+
 document.addEventListener("DOMContentLoaded", ready);
+
 function ready(){
     createLayout();
 }
 
 function createLayout () {
-    const header = document.getElementById('header');
-    const headerContainer = document.createElement('div');
+    header = document.getElementById('header');
+    headerContainer = document.createElement('div');
     header.appendChild(headerContainer);
 
-    const footer = document.getElementById('footer');
-    const footerContainer = document.createElement('div');
+    const headerTitle = document.createElement('h1');
+    const headerTitleText = document.createTextNode('Госпиталь «Андреевский»');
+    headerTitle.appendChild(headerTitleText);
+    headerContainer.appendChild(headerTitle);
+
+    footer = document.getElementById('footer');
+    footerContainer = document.createElement('div');
     footerContainer.style.textAlign = 'center';
     footer.appendChild(footerContainer);
     const copyright =  document.createElement('p');
-    const node = document.createTextNode('© Оформление сайта. ООО «Ромашка», 2020');
+    const node = document.createTextNode('© Оформление сайта. ООО «Ромашка», 2024');
     copyright.appendChild(node);
     copyright.style.color = '#FFFAF0';
-    footerContainer.appendChild(copyright)
+    footerContainer.appendChild(copyright);
 }
--- public_html/js/personal.js
+++ public_html/js/personal.js
@@ -1,8 +1,28 @@
 
 function drawPersonalTable() {
-    fetch('./static/personal.json')
-        .then(response => response.json())
-        .then(data => console.log('data', data))
+    let personal = loadPersonal();
+    console.log('personal: ', personal );
+}
+
+async function loadPersonal(){
+    let personalResponse = null;
+    let personalJson = null
+    try {
+        personalResponse = await fetch('./static/personal.json')
+        personalJson =  await personalResponse.json()
+       /* personal = await fetch('./static/personal.json')
+            .then(response => response.json())
+            .then(json => {
+                console.log('json: ', json);
+                return json
+            })*/
+
+        console.log('uuu personalJson: ',personalJson );
+        return personalJson;
+    } catch(error) {
+        console.log(error);
+    }
+    return personalJson;
 }
 
 drawPersonalTable();
