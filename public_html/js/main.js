100b8213e3967379c83f83182de1a0111cb57d4f
Ruslan Makarov <rmakarov@sprinterra.ru>
2024-11-21 12:33:40 +0300

    Added loggedUser cookie

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,6 +4,45 @@ let headerContainer = null;
 let footer = null;
 let footerContainer = null;
 
+const LOGIN_USER_COOKIE_NAME = "loggedUser";
+function setCookie(name, value, options = {}) {
+
+    options = {
+        path: '/',
+        // при необходимости добавьте другие значения по умолчанию
+        ...options
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", {
+        'max-age': -1
+    })
+}
+
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready(){
